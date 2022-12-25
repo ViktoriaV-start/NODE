39ffd3eb61d8384a17fbf82367f74802a465dd4e
ViktoriaV <august.flamenco@gmail.com>
2022-12-25 21:54:51 +0300

    Lesson 2: emmiter

--- index.js
+++ index.js
@@ -1,76 +1,126 @@
+import EventEmitter from 'events';
 import colors from 'colors';
 
 
-const clr = ['red', 'yellow', 'green'];
-let relation = [true, false, false,];
+let [hour, day, month, year] = process.argv.slice(2);
 
-const args = process.argv.slice(2);
-let min = +args[0];
-let max = +args[1];
+const timer = {
+hour: Number(hour),
+day: Number(day),
+month: Number(month),
+year: Number(year),
+};
 
-if(min > max) {
-  min = +args[1];
-  max = +args[0]
-}
-
-
-
-function checkValue(el) {
-  if(el) return true;
-  return false;
-}
-
-function checkNum(num) {
-  for (let i = 2; i <= Math.ceil(num/2); i++) {
-    if (num % i === 0) return false;
+class TimerInfo {
+  constructor(type, payload) {
+    this.type = type;
+    this.payload = payload; }
   }
-  return num;
+
+class MyEmitter extends EventEmitter {};
+const emitterObject = new MyEmitter();
+
+const delay = () => {
+  return new Promise((resolve, reject) => {
+  setTimeout(resolve, 1500);
+  });
+};
+
+const getDaysInMonth = () => {
+  return new Date(timer.year, timer.month, 0).getDate();
 }
 
-function getPrime(min, max) {
-  if (isNaN(min) || isNaN(max)) {
-    console.log('No prime numbers');
+const checkTimer = () => {
+
+  let type = null;
+  let info = {
+    hours: null,
+    days: null,
+    months: null,
+    years: null
+  };
+
+  const now = new Date();
+
+  const currentDate = {
+    currentHour: now.getHours(),
+    currentDay: now.getDate(),
+    currentMonth: now.getMonth() + 1,
+    currentYear: now.getFullYear(),
+  };
+
+    if (currentDate.currentHour > timer.hour) {
+      info.hours = 24 - currentDate.currentHour + timer.hour ;
+      info.days = timer.day - currentDate.currentDay - 1;
+    } else {
+      info.hours = timer.hour - currentDate.currentHour;
+      info.days = timer.day -currentDate.currentDay;
+    }
+
+    info.months = timer.month - currentDate.currentMonth;
+    info.years = timer.year - currentDate.currentYear;
+
+  if (currentDate.currentYear > timer.year || 
+      timer.month > 12 ||
+      timer.month <= 0 || 
+      timer.day > 31 ||
+      info.days < 0 ||
+      timer.day > getDaysInMonth() ||
+      currentDate.currentDay > timer.day ||
+      timer.day <= 0) {
+      type = 'out';
+  } else if (info.hours === 0 &&
+             info.days === 0 &&
+             info.months === 0 &&
+             info.years === 0) {
+      type = 'stop';
+  } else {
+      type = 'inWork';
+  }
+
+  return delay().then(() => {
+    let timerInfo = new TimerInfo(type, info);
+    return (timerInfo);
+  });
+
+}
+
+class Handler {
+  static inWork(payload) {
+    console.log(`Time to stop: ${payload.hours} hours - ${payload.days} days - ${payload.months} months - ${payload.years} years`);
+  }
+  static stop() {
+    console.log(colors.bgCyan('Stop'));
+  }
+  static out() {
+  console.log(colors.red('Out of Timer'));
+  }
+}
+
+emitterObject.on('inWork', Handler.inWork);
+emitterObject.once('out', Handler.out);
+emitterObject.once('stop', Handler.stop);
+
+// ВОТ В ТАКОМ ВИДЕ НЕ РАБОТАЕТ
+  // const run = () => {
+  //   checkTimer().then(
+  //     (timerInfo) => emitterObject.emit(timerInfo.type, timerInfo.payload)
+  //     )
+  //   // if (timerInfo.type !== 'inWork') {
+  //   //   return;
+  //   // }
+
+  //   run();
+  // };
+
+const run = async () => {
+  const timerInfo = await checkTimer(); 
+  emitterObject.emit(timerInfo.type, timerInfo.payload);
+  if (timerInfo.type !== 'inWork') {
     return;
   }
-  
-  let arr = [];
+  run(); 
+};
 
-  for (let i = min; i <= max; i++){
-    if (i <= 1) continue;
-    let result = checkNum(i);
-    if(result) {
-      arr.push(result);
-    }
-  }
-
-  if (arr.length === 0) {
-    console.log('No prime numbers')
-  } else {
-    arr.forEach((el) => {
-      showWithColor(el);
-    });
-  }
-}
-
-function showWithColor(el) {
-
-let idx = relation.findIndex(checkValue);
-console.log(colors[clr[idx]](el), clr[idx]);
-
-relation[idx] = false;
-
-if ( idx < (relation.length-1) ) {
-  relation[idx+1] = true;
-} else {
-  relation [0] = true;
-}
-}
-
-getPrime(min, max);
-
-
-
-
-
-
+run();
 
